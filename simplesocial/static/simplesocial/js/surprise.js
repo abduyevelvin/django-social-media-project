384e8a7259dab837ceb555c8387c29c062954e88
ABDU104 <elvin.abduyev@bertelsmann.de>
2020-04-11 23:53:59 +0200

    social media project in django

---
+++ simplesocial/static/simplesocial/js/surprise.js
@@ -0,0 +1,5 @@
+var d = new Date();
+var days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+if (d.getDay() == 6) {
+	document.getElementById("demo").innerHTML = "Günel, bu gün dünenden daha gözelsen..."
+};
